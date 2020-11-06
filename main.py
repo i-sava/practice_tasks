e2a3f835e4daaefa952dfa5493ce6d2e19a80304
isava1 <voynyliv@ukr.net>
2020-11-06 10:08:42 +0000

    add solve of problem 2

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-#
-#
-#
+"""
+Задається послідовність чисел з клавіатури.
+Знайти всі числа більші за 5 та збільшити їх вдвічі, вивести їх та обчислити їхню суму
+"""
+
+lst = list(map(int,input('введіть числа через пробіл у список   ').split()))
+print(lst)
+
+lst2 = [x for x in lst if x > 5]
+print(lst2)
+
+s = 0
+for i in range(len(lst)):
+  if lst[i] > 5:
+    lst[i] = lst[i] * 2
+  s = s + lst[i]
+print(lst)
+print(s)
+print(sum(lst))
+
+lst2 = [x for x in lst if x > 5]
+print(lst2)
